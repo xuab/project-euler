64d37997244e1723410c14d87985252fcc98cdb2
Stéphane Clairbaux <stephane@clairbaux.com>
2020-12-02 21:09:56 +0100

    add rust benchmarks

--- rust/src/index.rs
+++ rust/src/index.rs
@@ -1,1 +1,16 @@
+#![feature(test)]
+extern crate test;
 mod p001;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use super::p001::*;
+    use test::Bencher;
+
+    #[bench]
+    fn bench_p001(b: &mut Bencher) {
+        assert_eq!(233168, p001());
+        b.iter(|| p001());
+    }
+}
