17e5c5c7c31938cd5e24c94cc639453e3afba42d
Stéphane Clairbaux <stephane@clairbaux.com>
2020-12-02 20:59:35 +0100

    add descriptions in comments

--- javascript/p003.js
+++ javascript/p003.js
@@ -1,3 +1,6 @@
+// The prime factors of 13195 are 5, 7, 13 and 29.
+// What is the largest prime factor of the number 600851475143 ?
+
 exports.p003 = () => {
   let j = 600_851_475_143
   let i = 2
